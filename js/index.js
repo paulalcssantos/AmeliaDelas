580df91bbc4fdf7e98d50f9a0f611fc0c0f18b9a
Paula Luiza <108904097+paulalcssantos@users.noreply.github.com>
2022-09-24 14:51:30 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,11 @@
+window.addEventListener("dfMessengerLoaded", function (event) {
+    const dfMessenger = document.querySelector("df-messenger"); 
+    const style = document.createElement("style");
+
+    const nonMobileMinWidth = 601;
+
+    style.textContent = "@media screen and (min-width: " + nonMobileMinWidth + "px) { .chat-wrapper { max-height: 80% } }";
+
+    dfMessenger.shadowRoot.querySelector("df-messenger-chat").shadowRoot.appendChild(style);
+
+})
